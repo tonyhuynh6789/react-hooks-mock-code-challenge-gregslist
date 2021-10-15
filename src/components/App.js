4606a7997fa61cd0d99f6544e112f77681f9f1d1
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2021-10-14 17:46:16 -0700

    commit tree 3af2fcc56825bebbfbabaef7db5181b0b112d974

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,10 @@ import ListingsContainer from "./ListingsContainer";
 function App() {
   return (
     <div className="app">
+
       <Header />
-      <ListingsContainer />
+         <ListingsContainer />
     </div>
   );
 }
-
 export default App;
